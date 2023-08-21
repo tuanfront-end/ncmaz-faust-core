5b3410202f352bc20f83c7f1e6f66c76fee27b0b
Le Anh Tuan <leanhtuan@Les-MacBook-Pro.local>
2023-08-21 11:31:44 +0700

    aa

--- includes/ncmaz-custom-wpgraphql.php
+++ includes/ncmaz-custom-wpgraphql.php
@@ -15,32 +15,18 @@
 
 
 
-// add_filter('graphql_post_object_connection_query_args', function ($query_args, $source, $input) {
-//     if (isset($input['where']['orderby']) && is_array($input['where']['orderby'])) {
-//         foreach ($input['where']['orderby'] as $orderby) {
-//             if (!isset($orderby['field']) || 'reaction_liked_list' !== $orderby['field']) {
-//                 continue;
-//             }
-//             $query_args['meta_key'] = 'reaction_liked_list';
-//             $query_args['orderby'] = 'meta_value_num';
-//             $query_args['order'] = $orderby['order'];
-//         }
-//     }
-//     return $query_args;
-// }, 10, 3);
-
 // MAKE ALL AUTHOR IS PUBLIC
-// add_filter('graphql_connection_query_args', function ($query_args, $connection_resolver) {
-//     if ($connection_resolver instanceof \WPGraphQL\Data\Connection\UserConnectionResolver) {
-//         unset($query_args['has_published_posts']);
-//     }
-//     return $query_args;
-// }, 10, 2);
+add_filter('graphql_connection_query_args', function ($query_args, $connection_resolver) {
+    if ($connection_resolver instanceof \WPGraphQL\Data\Connection\UserConnectionResolver) {
+        unset($query_args['has_published_posts']);
+    }
+    return $query_args;
+}, 10, 2);
 
-// add_filter('graphql_object_visibility', function ($visibility, $model_name, $data, $owner, $current_user) {
-//     // only apply our adjustments to the UserObject Model
-//     if ('UserObject' === $model_name) {
-//         $visibility = 'public';
-//     }
-//     return $visibility;
-// }, 10, 5);
+add_filter('graphql_object_visibility', function ($visibility, $model_name, $data, $owner, $current_user) {
+    // only apply our adjustments to the UserObject Model
+    if ('UserObject' === $model_name) {
+        $visibility = 'public';
+    }
+    return $visibility;
+}, 10, 5);
