a1f36591936cd99672e1a06b506957d113980130
Chis Nghiax <atuanvptd@gmail.com>
2024-06-09 11:55:37 +0700

    update buid

--- build/block-magazine/index.asset.php
+++ build/block-magazine/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '9b91238ffd4f878f9646');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'c4740123a6e566848f0a');
