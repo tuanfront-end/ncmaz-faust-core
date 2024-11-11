22b223d5fd885ea9edfc8f00d80ca9067cd1f1e2
Chis Nghiax <atuanvptd@gmail.com>
2024-11-11 15:43:11 +0700

    update v2.7.0 support WPGRAPHQL_CONTENT_BLOCKS_VERSION 4.2.0

--- ncmaz-faust-core.php
+++ ncmaz-faust-core.php
@@ -5,7 +5,7 @@
  * Description:       Core for Ncmaz headless!
  * Requires at least: 5.8
  * Requires PHP:      7.3
- * Version:           2.6.0
+ * Version:           2.7.0
  * Author:            ChisNghiax
  * Author URI:        https://ChisNghiax.com/
  * License:           GPL-2.0-or-later
@@ -17,7 +17,7 @@
 
 defined('ABSPATH') || exit;
 
-define('NCMAZFC_VERSION', '2.6.0');
+define('NCMAZFC_VERSION', '2.7.0');
 define('NCMAZFC_FILE', __FILE__);
 define('NCMAZFC_PLUGIN_BASE', plugin_basename(NCMAZFC_FILE));
 define('NCMAZFC_PATH', plugin_dir_path(NCMAZFC_FILE));
@@ -40,7 +40,7 @@ require plugin_dir_path(__FILE__) . 'includes/ncmazfc-AFC-fields.php';
 
 add_action('plugins_loaded',  function () {
     if (
-        class_exists('WPGraphQL') && class_exists('ACF') && defined('FAUSTWP_FILE') && defined('WPGRAPHQL_FOR_ACF_VERSION') && defined('WPGRAPHQL_SMART_CACHE_VERSION') && defined('WPGRAPHQL_CONTENT_BLOCKS_DIR') && defined('MAILPOET_VERSION')
+        class_exists('WPGraphQL') && class_exists('ACF') && defined('FAUSTWP_FILE') && defined('WPGRAPHQL_FOR_ACF_VERSION') && defined('WPGRAPHQL_SMART_CACHE_VERSION') && (defined('WPGRAPHQL_CONTENT_BLOCKS_DIR') || defined('WPGRAPHQL_CONTENT_BLOCKS_VERSION')) && defined('MAILPOET_VERSION')
     ) {
         require plugin_dir_path(__FILE__) . 'includes/ncmazfc-wpgraphql-mutation.php';
         require plugin_dir_path(__FILE__) . 'includes/ncmazfc-wpgraphql-custom-where.php';
@@ -60,5 +60,5 @@ add_action('plugins_loaded',  function () {
         return;
     }
     // 
-});
+}, 999);
 // end-----------------
